8850d1b132a70083ea354584d92ac3c014d13937
ashokdpgk-wq <ashokdpgk@gmail.com>
2026-09-08 15:52:25 +0530

    Add voter data to voter_110_backup.js

--- voter_110_backup.js
+++ voter_110_backup.js
@@ -1,1 +1,56 @@
-
+[
+  {
+    "serial": "1",
+    "name": "SANEKHA ROY",
+    "relationType": "HUSBAND",
+    "relation": "SANJIT ROY",
+    "house": "n0063",
+    "dob": "36",
+    "epic": "JLG3685500"
+  },
+  {
+    "serial": "2",
+    "name": "AJIT ROY",
+    "relationType": "FATHER",
+    "relation": "PARIMAL ROY",
+    "house": "n0081",
+    "dob": "25",
+    "epic": "SKW2191245"
+  },
+  {
+    "serial": "3",
+    "name": "SOMA ROY",
+    "relationType": "FATHER",
+    "relation": "SHAILEN ROY",
+    "house": "n0066",
+    "dob": "22",
+    "epic": "SKW2624294"
+  },
+  {
+    "serial": "4",
+    "name": "BHASHWATI ROY",
+    "relationType": "HUSBAND",
+    "relation": "SUSHANTA KUMAR ROY",
+    "house": "n0290",
+    "dob": "35",
+    "epic": "SKW1164417"
+  },
+  {
+    "serial": "5",
+    "name": "KAILASH BARMAN",
+    "relationType": "FATHER",
+    "relation": "KAMALA KANTA BARMAN",
+    "house": "n0090",
+    "dob": "50",
+    "epic": "JLG3743515"
+  },
+  {
+    "serial": "6",
+    "name": "SHAILEN ROY",
+    "relationType": "FATHER",
+    "relation": "GAJEN ROY",
+    "house": "n0085",
+    "dob": "54",
+    "epic": "JLG3743473"
+  }
+]
