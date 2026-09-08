dc36298e099b2684774ff54e4096e60311977f51
ashokdpgk-wq <ashokdpgk@gmail.com>
2026-09-08 22:13:15 +0530

    Update aadhaar_backup_magurmari_I.js

--- aadhaar_backup_magurmari_I.js
+++ aadhaar_backup_magurmari_I.js
@@ -1,1 +1,416 @@
+window.aadhaarData = [
+  {
+    "name": "Ashok Sarkar",
+    "aadhaar": "792365987630",
+    "relationType": "Father",
+    "relationName": "BIBHUTI SARKAR",
+    "format": "7923-6598-7630"
+  },
+  {
+    "name": "JAYDEB MAHANTA",
+    "aadhaar": "373074309679",
+    "relationType": "Father",
+    "relationName": "PRADEEP MAHANTA",
+    "format": "3730-7430-9679"
+  },
+  {
+    "name": "BISHNU ROY",
+    "aadhaar": "311054732799",
+    "relationType": "Father",
+    "relationName": "PRAFULLA ROY",
+    "format": "3110-5473-2799"
+  },
+  {
+    "name": "SAMIR CHAKRABORTY",
+    "aadhaar": "311054732799",
+    "relationType": "Father",
+    "relationName": "SADHAN CHAKRABORTY",
+    "format": "5056-4450-8277"
+  },
+  {
+    "name": "ABHIJIT ROY",
+    "aadhaar": "274150647542",
+    "relationType": "Father",
+    "relationName": "ASHIRAM ROY",
+    "format": "2741-5064-7542"
+  },
+  {
+    "name": "ABHIJIT ROY",
+    "aadhaar": "800703262156",
+    "relationType": "Father",
+    "relationName": "MANIK ROY",
+    "format": "8007-0326-2156"
+  },
+  {
+    "name": "AHALLYA ROY",
+    "aadhaar": "262832145380",
+    "relationType": "Father",
+    "relationName": "SHAMBHU ROY",
+    "format": "2628-3214-5380"
+  },
+  {
+    "name": "ALAKA ROY",
+    "aadhaar": "410559645614",
+    "relationType": "Husband",
+    "relationName": "NIRENDRA ROY",
+    "format": "4105-5964-5614"
+  },
+  {
+    "name": "ANITA ROY",
+    "aadhaar": "836035493464",
+    "relationType": "Father",
+    "relationName": "BHABANI ROY",
+    "format": "8360-3549-3464"
+  },
+  {
+    "name": "ASALATA MAJUMDAR",
+    "aadhaar": "548083128433",
+    "relationType": "Father",
+    "relationName": "AKHAY MAJUMD",
+    "format": "5480-8312-8433"
+  },
+  {
+    "name": "BAPPI ROY",
+    "aadhaar": "301418471150",
+    "relationType": "Father",
+    "relationName": "KRISHNA PADA ROY",
+    "format": "3014-1847-1150"
+  },
+  {
+    "name": "BHARATIRANI SARKAR",
+    "aadhaar": "806879093219",
+    "relationType": "Husband",
+    "relationName": "MRINAL KANTI SARKAR",
+    "format": "8068-7909-3219"
+  },
+  {
+    "name": "AMAL KR ROY",
+    "aadhaar": "590655738413",
+    "relationType": "Father",
+    "relationName": "JAGMOHAN ROY",
+    "format": "5906-5573-8413"
+  },
+  {
+    "name": "SUCHITRA SARKAR",
+    "aadhaar": "943801812512",
+    "relationType": "Husband",
+    "relationName": "BIBHUTI SARKAR",
+    "format": "9438-0181-2512"
+  },
+  {
+    "name": "NILIMA SARKAR MODAK",
+    "aadhaar": "690924934669",
+    "relationType": "Husband",
+    "relationName": "KHOKAN SARKAR",
+    "format": "6909-2493-4669"
+  },
+  {
+    "name": "SADHANA ROY",
+    "aadhaar": "974789357216",
+    "relationType": "Husband",
+    "relationName": "SANTOSH KR ROY",
+    "format": "9747-8935-7216"
+  },
+  {
+    "name": "BIBEK ROY",
+    "aadhaar": "958431240224",
+    "relationType": "Father",
+    "relationName": "SANTOSH KR ROY",
+    "format": "9584-3124-0224"
+  },
+  {
+    "name": "JAY ROY",
+    "aadhaar": "428695525245",
+    "relationType": "Father",
+    "relationName": "SANTOSH KR ROY",
+    "format": "4286-9552-5245"
+  },
+  {
+    "name": "SANTOSH KUMAR ROY",
+    "aadhaar": "760923200335",
+    "relationType": "Father",
+    "relationName": "NARENDRA NATH ROY",
+    "format": "7609-2320-0335"
+  },
+  {
+    "name": "RAJDIPTA ROY",
+    "aadhaar": "592115244195",
+    "relationType": "Father",
+    "relationName": "APU ROY",
+    "format": "5921-1524-4195"
+  },
+  {
+    "name": "PRATIMA ROY",
+    "aadhaar": "745144066409",
+    "relationType": "Husband",
+    "relationName": "APU ROY",
+    "format": "7451-4406-6409"
+  },
+  {
+    "name": "SANJAY ROU",
+    "aadhaar": "907438225834",
+    "relationType": "Father",
+    "relationName": "NARESH ROY",
+    "format": "9074-3822-5834"
+  },
+  {
+    "name": "ATUL CHANDRA ROY",
+    "aadhaar": "566326696121",
+    "relationType": "Father",
+    "relationName": "NARENDRA NATH ROY",
+    "format": "5663-2669-6121"
+  },
+  {
+    "name": "ALOK ROY",
+    "aadhaar": "507414403144",
+    "relationType": "Father",
+    "relationName": "ATUL CHANDRA ROY",
+    "format": "5074-1440-3144"
+  },
+  {
+    "name": "SANGITA MAJUMDER RAHA",
+    "aadhaar": "804084275231",
+    "relationType": "C/O",
+    "relationName": "SAJAL RAHA",
+    "format": "8040-8427-5231"
+  },
+  {
+    "name": "MADHAB SARKAR",
+    "aadhaar": "512879416618",
+    "relationType": "Father",
+    "relationName": "MANINDRA SARKAR",
+    "format": "5128-7941-6618"
+  },
+  {
+    "name": "ASIT SARKAR",
+    "aadhaar": "735306255352",
+    "relationType": "Father",
+    "relationName": "MADHAB SARKAR",
+    "format": "7353-0625-5352"
+  },
+  {
+    "name": "GITA MONDAL SARKAR",
+    "aadhaar": "200737481325",
+    "relationType": "C/O",
+    "relationName": "",
+    "format": "2007-3748-1325"
+  },
+  {
+    "name": "NANIGOPAL KIRTONIYA",
+    "aadhaar": "658800864310",
+    "relationType": "Father",
+    "relationName": "KALICHARAN KIRTONIYA",
+    "format": "6588-0086-4310"
+  },
+  {
+    "name": "MAMATA KIRTONIYA",
+    "aadhaar": "365543483450",
+    "relationType": "Husband",
+    "relationName": "NANIGOPAL KIRTONIYA",
+    "format": "3655-4348-3450"
+  },
+  {
+    "name": "SHANTO KIRTONIYA",
+    "aadhaar": "851477200377",
+    "relationType": "Father",
+    "relationName": "NANIGOPAL KIRTONIYA",
+    "format": "8514-7720-0377"
+  },
+  {
+    "name": "BISWAJIT KIRTONIA",
+    "aadhaar": "281016568651",
+    "relationType": "Father",
+    "relationName": "NANIGOPAL KIRTONIA",
+    "format": "2810-1656-8651"
+  },
+  {
+    "name": "SABITA ROY",
+    "aadhaar": "724575243786",
+    "relationType": "Husband",
+    "relationName": "BISWAJIT KIRTONIA",
+    "format": "7245-7524-3786"
+  },
+  {
+    "name": "JYOTSNA ROY",
+    "aadhaar": "203916859604",
+    "relationType": "Husband",
+    "relationName": "DHIRENDRA NATH ROY",
+    "format": "2039-1685-9604"
+  },
+  {
+    "name": "DEBBRATA ROY",
+    "aadhaar": "848772006383",
+    "relationType": "Father",
+    "relationName": "DHIRENDRA NATH ROY",
+    "format": "8487-7200-6383"
+  },
+  {
+    "name": "SIKHA SARKAR",
+    "aadhaar": "424154156351",
+    "relationType": "Father",
+    "relationName": "SHYAMAL SARKAR",
+    "format": "4241-5415-6351"
+  },
+  {
+    "name": "MOUMITA ADHIKARI",
+    "aadhaar": "254118844975",
+    "relationType": "Mother",
+    "relationName": "SHYAMALI ADHIKARI ROY",
+    "format": "2541-1884-4975"
+  },
+  {
+    "name": "MINTU ROY",
+    "aadhaar": "864879924927",
+    "relationType": "Father",
+    "relationName": "",
+    "format": "8648-7992-4927"
+  },
+  {
+    "name": "MALOTI SARKAR",
+    "aadhaar": "318992786840",
+    "relationType": "Husband",
+    "relationName": "SUBAL BISWAS",
+    "format": "3189-9278-6840"
+  },
+  {
+    "name": "SARALA BALA BISWAS",
+    "aadhaar": "207185448266",
+    "relationType": "Husband",
+    "relationName": "KHAGENDRA NATH BISWAS",
+    "format": "2071-8544-8266"
+  },
+  {
+    "name": "TIKLI MAHANTA",
+    "aadhaar": "218219561263",
+    "relationType": "Father",
+    "relationName": "NANTU MAHANTA",
+    "format": "2182-1956-1263"
+  },
+  {
+    "name": "BINAY BHUSHAN SARKAR",
+    "aadhaar": "263017144802",
+    "relationType": "Father",
+    "relationName": "BIPIN CHANDRA SARKAR",
+    "format": "2630-1714-4802"
+  },
+  {
+    "name": "TAPAN SARKAR",
+    "aadhaar": "379045261041",
+    "relationType": "Father",
+    "relationName": "RANJIT SARKAR",
+    "format": "3790-4526-1041"
+  },
+  {
+    "name": "JANA SARKAR",
+    "aadhaar": "368265395597",
+    "relationType": "Father",
+    "relationName": "BIDHAN CHANDRA SARKAR",
+    "format": "3682-6539-5597"
+  },
+  {
+    "name": "ANKUSH SARKAR",
+    "aadhaar": "589539985467",
+    "relationType": "Father",
+    "relationName": "SANJAY SARKAR",
+    "format": "5895-3998-5467"
+  },
+  {
+    "name": "AYAN SARKAR",
+    "aadhaar": "940936262644",
+    "relationType": "Father",
+    "relationName": "CHANDAN SARKAR",
+    "format": "9409-3626-2644"
+  },
+  {
+    "name": "KEYAN SARKAR",
+    "aadhaar": "938315653536",
+    "relationType": "Father",
+    "relationName": "MANI SARKAR",
+    "format": "9383-1565-3536"
+  },
+  {
+    "name": "ANWESHA DEB",
+    "aadhaar": "693417493268",
+    "relationType": "Father",
+    "relationName": "RAMEN DEB",
+    "format": "6934-1749-3268"
+  },
+  {
+    "name": "ARNAB SARKAR",
+    "aadhaar": "656974196322",
+    "relationType": "Father",
+    "relationName": "DIPU SARKAR",
+    "format": "6569-7419-6322"
+  },
+  {
+    "name": "PRAMILA ROY",
+    "aadhaar": "889341642862",
+    "relationType": "Husband",
+    "relationName": "ANIL CHANDRA ROY",
+    "format": "8893-4164-2862"
+  },
+  {
+    "name": "RUPALI ROY",
+    "aadhaar": "739414477281",
+    "relationType": "Husband",
+    "relationName": "BISHWAJIT ROY",
+    "format": "7394-1447-7281"
+  },
+  {
+    "name": "ANIL CHANDRA ROY",
+    "aadhaar": "569960012565",
+    "relationType": "Father",
+    "relationName": "",
+    "format": "5699-6001-2565"
+  },
+  {
+    "name": "RAKHAL BISWAS",
+    "aadhaar": "795969005898",
+    "relationType": "Father",
+    "relationName": "KHAGENDRA NATH BISWAS",
+    "format": "7959-6900-5898"
+  },
+  {
+    "name": "ARUP SARKAR",
+    "aadhaar": "524755394360",
+    "relationType": "Father",
+    "relationName": "CHANDAN SARKAR",
+    "format": "5247-5539-4360"
+  },
+  {
+    "name": "ANINDA SARKAR",
+    "aadhaar": "886821534211",
+    "relationType": "Father",
+    "relationName": "SHASHANKA SARKAR",
+    "format": "8868-2153-4211"
+  },
+  {
+    "name": "SUSMITA SARKAR",
+    "aadhaar": "614426034576",
+    "relationType": "Father",
+    "relationName": "JAGADISH SARKAR",
+    "format": "6144-2603-4576"
+  },
+  {
+    "name": "BISWAJIT ROY",
+    "aadhaar": "465624373660",
+    "relationType": "Father",
+    "relationName": "LONKESWAR ROY",
+    "format": "4656-2437-3660"
+  },
+  {
+    "name": "SUDHARANI ROY",
+    "aadhaar": "610860276197",
+    "relationType": "Husband",
+    "relationName": "KOKAN ROY",
+    "format": "6108-6027-6197"
+  },
+  {
+    "name": "SANKAR SARKAR",
+    "aadhaar": "291074970113",
+    "relationType": "Father",
+    "relationName": "",
+    "format": "2910-7497-0113"
+  }
+];
 
