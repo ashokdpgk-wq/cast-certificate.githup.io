646899343088b93b4f91c40ecf9f08f32c647d1a
ashokdpgk-wq <ashokdpgk@gmail.com>
2026-09-09 09:25:24 +0530

    Add aadhaar data for multiple individuals

--- aadhaar_backup_gadheyarkuthi.js
+++ aadhaar_backup_gadheyarkuthi.js
@@ -1,1 +1,60 @@
+window.aadhaarData = [
+
+  {
+    "name": "BIMAL CHANDRA ROY",
+    "relationType": "Father",
+    "relationName": "",
+    "raw": "286608992583",
+    "format": "2866-0899-2583"
+  },
+  {
+    "name": "BIMLABALA ROY",
+    "relationType": "Husband",
+    "relationName": "",
+    "raw": "229115130277",
+    "format": "2291-1513-0277"
+  },
+  {
+    "name": "PUSHPITA ROY",
+    "relationType": "Father",
+    "relationName": "JAGANANDA ROY",
+    "raw": "281722681485",
+    "format": "2817-2268-1485"
+  },
+  {
+    "name": "CHITRA MAHANTA",
+    "relationType": "Husband",
+    "relationName": "PRADIP MAHANTA",
+    "raw": "660809362818",
+    "format": "6608-0936-2818"
+  },
+  {
+    "name": "RANI MANDAL",
+    "relationType": "Father",
+    "relationName": "SHRI NANDA MANDAL",
+    "raw": "789497324961",
+    "format": "7894-9732-4961"
+  },
+  {
+    "name": "RATAN ROY",
+    "relationType": "Father",
+    "relationName": "",
+    "raw": "476637731052",
+    "format": "4766-3773-1052"
+  },
+  {
+    "name": "ARINDAM MODAK",
+    "relationType": "Father",
+    "relationName": "ABINASH MODAK",
+    "raw": "557080428105",
+    "format": "5570-8042-8105"
+  },
+  {
+    "name": "KARNADEB ROY",
+    "relationType": "Father",
+    "relationName": "BIMAL CHANDRA ROY",
+    "raw": "263456547212",
+    "format": "2634-5654-7212"
+  }
+];
 
